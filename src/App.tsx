0385220f9ab2200d90fed3fc3ca79a329e5f6ddc
Tiago Braga <ztiagok@gmail.com>
2026-08-27 15:51:50 -0300

    feat: Creating "Tasks" components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
 const App = () => {
+  const [tasks, _setTasks] = useState([
+    { id: 1, title: "Estudar", description: "Teste", isCompleted: false },
+    { id: 2, title: "Comer", description: "Teste", isCompleted: false },
+    { id: 3, title: "Treinar", description: "Teste", isCompleted: false },
+  ]);
+
   return (
-    <div>
-      <Tasks />
-      <AddTask />
+    <div className="h-screen w-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+
+        <AddTask />
+        <Tasks tasks={tasks} />
+      </div>
     </div>
   );
 };
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,5 +1,27 @@
-const Tasks = () => {
-  return <div className="">Tasks OK</div>;
+const Tasks = ({ tasks }: Props) => {
+  return (
+    <ul className="space-y-3 p-6 bg-slate-200 rounded-md shadow">
+      {tasks.map((task) => (
+        <li className="flex" key={"Task-" + task.id}>
+          <button className="bg-slate-400 flex-1 text-white p-2 rounded-md">
+            {task.title}
+          </button>
+          <button>Ver Detalhes</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+interface Props {
+  tasks: TypeTasks[];
+}
+
+type TypeTasks = {
+  id: number;
+  title: string;
+  description: string;
+  isCompleted: boolean;
 };
 
 export default Tasks;
