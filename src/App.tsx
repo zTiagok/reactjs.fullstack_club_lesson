500ad0e9ce0cbecec243f6eeb2549d8a22510599
Tiago Braga <ztiagok@gmail.com>
2026-08-27 15:26:10 -0300

    feat: Starting project installing Vite+ReactJS and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+
+const App = () => {
+  return (
+    <div>
+      <Tasks />
+      <AddTask />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Tasks.tsx
@@ -0,0 +1,5 @@
+const Tasks = () => {
+  return <div className="">Tasks OK</div>;
+};
+
+export default Tasks;
